432f3bd45f023c851ebe7f91c2896f6d453fc40d
Sherman61 <shermanshiya@gmail.com>
2024-03-28 00:11:25 -0400

    switch case + lamda fumctions

---
+++ case.js
@@ -0,0 +1,22 @@
+const isEven = (randNum)=> randNum % 2 === 0;
+
+
+
+ 
+let randNum  = Math.floor(Math.random() * 1000)  + 1 ; 
+let currentYear = new Date().getFullYear() 
+const isGreaterThanYear =(num, currentYear) => num > currentYear
+
+console.log(`the year ${currentYear} is ${isGreaterThanYear(randNum, currentYear) ?'less' : 'more'} then the random number ${randNum}`)  
+
+switch(randNum){
+    case isGreaterThanYear(randNum, currentYear): //true or false
+        console.log('greater than year')
+    case isEven(randNum):// if even then true
+        console.log('even')
+    default:// otherwise (if odd && smaller than year) outpt the following
+        console.log('none of the above')
+}
+
+let x = 4;
+
