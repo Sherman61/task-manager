d5abc10b3568d7b441d93da7d7b9c13eb5f916ac
Sherman61 <shermanshiya@gmail.com>
2024-04-17 23:35:49 -0400

    refactored calculator

--- case.js
+++ case.js
@@ -9,13 +9,13 @@ const isGreaterThanYear =(num, currentYear) => num > currentYear
 
 console.log(`the year ${currentYear} is ${isGreaterThanYear(randNum, currentYear) ?'less' : 'more'} then the random number ${randNum}`)  
 
-switch(randNum){
+switch(true){
     case isGreaterThanYear(randNum, currentYear): //true or false
-        console.log('greater than year')
+        console.log('greater than year')//if true (is even) then brea
     case isEven(randNum):// if even then true
         console.log('even')
     default:// otherwise (if odd && smaller than year) outpt the following
-        console.log('none of the above')
+        console.log('none of the above') 
 }
 
 let x = 4;
